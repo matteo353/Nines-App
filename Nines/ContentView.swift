4151f37eafc17c7522902b2a6fe77f55c046c85b
MoAlemi <MoAliAlemi@gmail.com>
2023-10-30 23:48:51 -0400

    Scorecard and Input Section

--- Nines/ContentView.swift
+++ Nines/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  Nines
 //
-//  Created by Matteo Mastandrea on 10/30/23.
+//  Created by Matteo Mastandrea and Mohammad Alemi on 10/30/23.
 //
 
 import SwiftUI
@@ -92,7 +92,7 @@ struct AddPlayers: View {
                         
             Spacer()
             
-            NavigationLink(destination: PlayGame(), isActive: $navigateToPlay) {
+            NavigationLink(destination: PlayGame(playerNames: playerNames, selectedHoles: selectedHoles), isActive: $navigateToPlay) {
                 Text("Start Round")
                     .padding()
                     .background(Color.blue)
@@ -133,10 +133,105 @@ struct TwoSidedButton: View {
 }
 
 struct PlayGame: View {
-    var body: some View{
-        Text("Playing game")
+    var playerNames: [String]
+    var selectedHoles: String
+    var holeCount: Int {
+        return selectedHoles == "9 holes" ? 9 : 18
+    }
+    
+    @State private var currentHole = 1
+    @State private var holeScores: [[Int]] = Array(repeating: Array(repeating: 0, count: 18), count: 3)
+    @State private var currentStrokes: [Int] = Array(repeating: 0, count: 3)
+
+    var body: some View {
+        VStack {
+            // Scoreboard Section
+            ScrollView([.horizontal, .vertical]) {
+                VStack {
+                    // Scoreboard Header
+                    HStack {
+                        Text("Player")
+                            .frame(width: 80)
+                        
+                        ForEach(1...holeCount, id: \.self) { hole in
+                            Text("Hole \(hole)")
+                                .frame(width: 40)
+                        }
+                    }
+                    .font(.headline)
+                    
+                    // Scoreboard Rows
+                    ForEach(playerNames.indices, id: \.self) { index in
+                        ScoreRow(playerName: playerNames[index], holeScores: $holeScores[index], holeCount: holeCount)
+                    }
+                }
+                .padding()
+            }
+            
+            // Separator
+            Divider()
+            
+            
+            // Input Section
+            VStack {
+                Text("Hole \(currentHole):")
+                    .font(.headline)
+                
+                ForEach(playerNames.indices, id: \.self) { index in
+                    HStack {
+                        Text("\(playerNames[index]):")
+                        TextField("Strokes", value: $currentStrokes[index], formatter: NumberFormatter())
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .frame(width: 60)
+                    }
+                }
+                
+                // Submit Hole Button
+                Button("Submit Hole") {
+
+                    for i in 0..<playerNames.count {
+                        holeScores[i][currentHole - 1] = currentStrokes[i]
+                    }
+                    
+                    
+                    // Move to the next hole
+                    if currentHole < holeCount {
+                        currentHole += 1
+                    }
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            }
+            .padding()
+        }
     }
 }
+
+struct ScoreRow: View {
+    var playerName: String
+    @Binding var holeScores: [Int]
+    var holeCount: Int
+
+    var body: some View {
+        HStack {
+            Text(playerName)
+                .frame(width: 80)
+
+            ForEach(0..<holeCount, id: \.self) { hole in
+                Text("\(holeScores[hole])")
+                    .frame(width: 40)
+                    .padding(.horizontal, 5)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+            }
+        }
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
